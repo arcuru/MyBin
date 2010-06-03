7126728334baaaa903439e58a2a9288266adf415
Patrick Jackson <PatrickSJackson@gmail.com>
2010-06-03 00:55:17 -0400

    Changed font in dwm/config.h

--- dwm/config.h
+++ dwm/config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const char font[]            = "-*-*-medium-*-*-*-14-*-*-*-*-*-*-*";
+static const char font[]            = "-*-terminus-*-*-*-*-14-*-*-*-*-*-*-*";
 static const char normbordercolor[] = "#000000";
 static const char normbgcolor[]     = "#cccccc";
 static const char normfgcolor[]     = "#000000";

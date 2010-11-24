a2ab88ffd3a6e83bfa669fd8b74ff71c5a79e2ba
Patrick Jackson <PatrickSJackson@gmail.com>
2010-11-24 17:03:23 -0500

    Fixed a small error with range function

--- Py_Projects/Euler/5.py
+++ Py_Projects/Euler/5.py
@@ -2,7 +2,7 @@ def Euler_5(num=20):
 	'''Find the smallest integer evenly divisible by all numbers up to the input
 	'''
 	factor_list = []
-	for x in range(1,num):
+	for x in range(1,num+1):
 		for i in factor_list:
 			if x % i == 0:
 				x //= i

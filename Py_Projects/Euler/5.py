8ce67ee2107f6fe58eef649b05ac2106adcee3a3
Patrick Jackson <PatrickSJackson@gmail.com>
2010-11-24 16:44:41 -0500

    Euler python 5

---
+++ Py_Projects/Euler/5.py
@@ -0,0 +1,17 @@
+def Euler_5(num=20):
+	'''Find the smallest integer evenly divisible by all numbers up to the input
+	'''
+	factor_list = []
+	for x in range(1,num):
+		for i in factor_list:
+			if x % i == 0:
+				x //= i
+		if x != 1:
+			factor_list.append(x)
+	total = 1
+	for x in factor_list:
+		total *= x
+	return total
+
+if __name__ == '__main__':
+	print(Euler_5())

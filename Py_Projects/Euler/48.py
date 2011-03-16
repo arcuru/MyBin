a275fb20b0fc501d49866ac02017af8a377a4c39
Patrick Jackson <PatrickSJackson@gmail.com>
2011-03-16 12:53:38 -0400

    More elegant solution to Py Euler 48

--- Py_Projects/Euler/48.py
+++ Py_Projects/Euler/48.py
@@ -1,4 +1,3 @@
-from functools import reduce
 def Euler_48(base=1000, cut=10):
     '''Find the last cut digits of 1^1 + 2^2 ... + base^base
     '''
@@ -6,7 +5,7 @@ def Euler_48(base=1000, cut=10):
     #for x in range(1, base+1):
     #	ans += (x ** x)
     #return ans % (10 ** cut)
-    return reduce(lambda i,j:i+j,[x**x for x in range(1,base+1)]) % 10 ** cut
+    return sum([x**x for x in range(1,base+1)]) % 10 ** cut
 
 if __name__ == '__main__':
     print(Euler_48())

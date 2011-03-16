9b67be29dc5d19f36a7808fbd2f444c864adf4dc
Patrick Jackson <PatrickSJackson@gmail.com>
2011-03-16 03:44:22 -0400

    Fixed bounds error in Euler48

--- Py_Projects/Euler/48.py
+++ Py_Projects/Euler/48.py
@@ -6,7 +6,7 @@ def Euler_48(base=1000, cut=10):
     #for x in range(1, base+1):
     #	ans += (x ** x)
     #return ans % (10 ** cut)
-    return reduce(lambda i,j:i+j,[x**x for x in range(1,base)]) % 10 ** cut
+    return reduce(lambda i,j:i+j,[x**x for x in range(1,base+1)]) % 10 ** cut
 
 if __name__ == '__main__':
     print(Euler_48())

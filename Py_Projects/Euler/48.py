7adf87e2baf753f1324514678f953d0b35cc0244
Patrick Jackson <PatrickSJackson@gmail.com>
2011-03-16 01:38:26 -0400

    Slight modification to Python euler 48

--- Py_Projects/Euler/48.py
+++ Py_Projects/Euler/48.py
@@ -4,8 +4,7 @@ def Euler_48(base=1000, cut=10):
 	ans = 0
 	for x in range(1, base+1):
 		ans += (x ** x)
-		ans %= 10 ** cut
-	return ans
+	return ans % (10 ** cut)
 
 if __name__ == '__main__':
 	print(Euler_48())

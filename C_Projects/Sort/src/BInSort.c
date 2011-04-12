b75c0c0d624f713758e049125e1d601d22a25b5e
Patrick Jackson <PatrickSJackson@gmail.com>
2011-04-12 12:06:07 -0400

    Modified BInSort to use typedef

--- C_Projects/Sort/src/BInSort.c
+++ C_Projects/Sort/src/BInSort.c
@@ -2,15 +2,15 @@
 
 static int current = 0;
 
-struct TreeNode {
+typedef struct TN {
     int value;
     int count;
-    struct TreeNode* rightChild;
-    struct TreeNode* leftChild;
-};
+    struct TN* rightChild;
+    struct TN* leftChild;
+} TreeNode;
 
-static struct TreeNode* MakeTree(int num) {
-    struct TreeNode* tree = (struct TreeNode*) (malloc) (sizeof (struct TreeNode));
+static TreeNode* MakeTree(int num) {
+    TreeNode* tree = (TreeNode*) (malloc) (sizeof (TreeNode));
     tree->value = num;
     tree->count = 1;
     tree->rightChild = NULL;
@@ -18,7 +18,7 @@ static struct TreeNode* MakeTree(int num) {
     return tree;
 }
 
-static void FreeTree(struct TreeNode* n) {
+static void FreeTree(TreeNode* n) {
     if (n->leftChild != NULL)
         FreeTree(n->leftChild);
     if (n->rightChild != NULL)
@@ -26,7 +26,7 @@ static void FreeTree(struct TreeNode* n) {
     free(n);
 }
 
-static void SaveTree(struct TreeNode* n, int* sorted) {
+static void SaveTree(TreeNode* n, int* sorted) {
     if (n->leftChild != NULL)
         SaveTree(n->leftChild, sorted);
     int i;
@@ -36,19 +36,19 @@ static void SaveTree(struct TreeNode* n, int* sorted) {
         SaveTree(n->rightChild, sorted);
 }
 
-static void Insert(struct TreeNode* n, int num) {
+static void Insert(TreeNode* n, int num) {
     if (num == n->value)
         ++n->count;
     else if (num > n->value) {
         if (n->rightChild == NULL) {
-            struct TreeNode* tree = MakeTree(num);
+            TreeNode* tree = MakeTree(num);
             n->rightChild = tree;
             return;
         }
         Insert(n->rightChild, num);
     } else if (num < n->value) {
         if (n->leftChild == NULL) {
-            struct TreeNode* tree = MakeTree(num);
+            TreeNode* tree = MakeTree(num);
             n->leftChild = tree;
             return;
         }
@@ -58,7 +58,7 @@ static void Insert(struct TreeNode* n, int num) {
 }
 
 int* InsertionSortBinary(int* d, int n) {
-    struct TreeNode* tree = MakeTree(d[0]);
+    TreeNode* tree = MakeTree(d[0]);
     int i;
     for (i = 1; i < n; i++)
         Insert(tree, d[i]);

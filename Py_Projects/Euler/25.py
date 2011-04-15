c7c7f930aad565c873f78aae7537dfb42e9281b1
Patrick Jackson <PatrickSJackson@gmail.com>
2011-04-15 18:51:17 -0400

    Fixed formatting

--- Py_Projects/Euler/25.py
+++ Py_Projects/Euler/25.py
@@ -1,14 +1,14 @@
 def Euler_25(length=1000):
-    '''Find the index of the first term in the Fibonacci sequence
-        to contain 1000 digits
-    '''
-    total = 0
-    (a, b, count) = (1, 1, 2)
-    while True:
-        (a, b, count) = (b, a+b, count+1)
-        if len(str(b)) >= length:
-            return count
-    raise Exception
+	'''Find the index of the first term in the Fibonacci sequence
+	to contain 1000 digits
+	'''
+	total = 0
+	(a, b, count) = (1, 1, 2)
+	while True:
+		(a, b, count) = (b, a+b, count+1)
+		if len(str(b)) >= length:
+			return count
+	raise Exception
 
 if __name__ == '__main__':
-    print(Euler_25())
+	print(Euler_25())

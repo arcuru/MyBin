b4f1fb044d35a8b4c76aca58aa1e51fa9b4c451e
Patrick Jackson <PatrickSJackson@gmail.com>
2011-04-15 19:30:36 -0400

    Wrote a speed up for Euler 2 Py

--- Py_Projects/Euler/2.py
+++ Py_Projects/Euler/2.py
@@ -2,14 +2,15 @@ def Euler_2(top=4000000):
 	'''Find the sum of all even-valued terms in the Fibonacci sequence
 	that do not exceed the input
 	'''
-	total = 0
-	(a, b) = (1, 1)
+	(a, b) = (1, 2)
+	total = b
 	while b <= top:
 		(a, b) = (b, a+b)
-		if b % 2 == 0 and b <= top:
+		(a, b) = (b, a+b)
+		(a, b) = (b, a+b)
+		if  b <= top:
 			total += b
 	return total
-
 
 if __name__ == '__main__':
 	print(Euler_2())

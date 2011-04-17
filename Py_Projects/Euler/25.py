3e7ad3ea7594d1a94bf3692d906d0ceb0e4eed04
Patrick Jackson <PatrickSJackson@gmail.com>
2011-04-16 20:18:00 -0400

    Removed extra variable

--- Py_Projects/Euler/25.py
+++ Py_Projects/Euler/25.py
@@ -2,7 +2,6 @@ def Euler_25(length=1000):
 	'''Find the index of the first term in the Fibonacci sequence
 	to contain 1000 digits
 	'''
-	total = 0
 	(a, b, count) = (1, 1, 2)
 	while True:
 		(a, b, count) = (b, a+b, count+1)

38db106f832d2517f7079f2f4d5edd9831be09ba
Patrick Jackson <PatrickSJackson@gmail.com>
2011-04-22 23:33:10 -0400

    Adde Py Euler 123

---
+++ Py_Projects/Euler/123.py
@@ -0,0 +1,17 @@
+from gmpy import mpz
+from primes import Primes
+def Euler_123(low = 10**10):
+	''' Returns the nth prime that has a remainder above low when using
+			the given formula
+	'''
+	p = Primes(10**6)
+	primes = p.pList()
+	for x in range(0,len(primes)):
+		n = mpz(x+1)
+		p = mpz(primes[x]-1)**n + mpz(primes[x]+1)**n
+		if p % mpz(primes[x])**2 > low:
+			return n
+
+if __name__ == '__main__':
+	print(Euler_123())
+

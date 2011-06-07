548c40dad2f49e14972f9a15299f1a8a87adfccf
Patrick Jackson <PatrickSJackson@gmail.com>
2011-06-06 23:54:50 -0400

    Minor improvements to Python style

--- Py_Projects/Euler/14.py
+++ Py_Projects/Euler/14.py
@@ -1,4 +1,3 @@
-from functools import reduce
 def Euler_14(maxx=1000000):
 	'''Find the number under the input max that produces the longest chain
 		while moving along a Collatz sequence
@@ -12,8 +11,8 @@ def Euler_14(maxx=1000000):
 			retval += 1
 		return retval
 
-	clist = [collatz(x) for x in range(maxx//2,maxx)]
-	return clist.index(reduce(lambda i,j: max(i,j), clist)) + maxx//2
+	clist = [collatz(x) if x%2 else 0 for x in range(maxx//2,maxx)]
+	return clist.index(max(clist)) + maxx//2
 
 if __name__ == '__main__':
 	print(Euler_14())

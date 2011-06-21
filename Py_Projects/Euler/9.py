ecadd122dca056d24baa142158ad500d59be40da
Patrick Jackson <PatrickSJackson@gmail.com>
2011-06-21 19:17:50 -0400

    Reworked Py Euler 9 for multiprocessing

--- Py_Projects/Euler/9.py
+++ Py_Projects/Euler/9.py
@@ -1,18 +1,22 @@
-def Euler_9(limit=1000):
-	'''Find the pythagorean triplet for which a+b+c=limit
-		Return the product of abc
-	'''
-	for a in range(1,limit):
-		a2 = a**2
-		for b in range(a,limit):
-			ab2 = a2 + b**2
-			c2 = (limit-a-b)**2
-			if ab2 > c2:
-				break
-			elif ab2 == c2:
-				return a*b*(limit-a-b)
+from multiprocessing import Pool,Pipe
 
-	raise ValueError('No answer for input value')
+p_conn, c_conn = Pipe()
+
+def f(a):
+	a2 = a**2
+	limit = 1000
+	for b in range(a,limit):
+		ab2 = a2 + b**2
+		c2 = (limit-a-b)**2
+		if ab2 > c2:
+			break
+		elif ab2 == c2:
+			c_conn.send(a*b*(limit-a-b))
+			c_conn.close()
 
 if __name__ == '__main__':
-	print(Euler_9())
+	pool = Pool(processes=4)
+	result = pool.map_async(f, range(1,1000))
+	out = p_conn.recv()
+	pool.terminate()
+	print(out)

4d6426ed9d708235fd849f915efa6de458dc3bc3
Patrick Jackson <PatrickSJackson@gmail.com>
2011-07-06 21:17:05 -0400

    Improved readability on Py Euler 14 and made it compatible with Pypy

--- Py_Projects/Euler/14.py
+++ Py_Projects/Euler/14.py
@@ -1,4 +1,4 @@
-def Euler_14(maxx=1000000):
+def Euler_14(maxx=10**6):
 	'''Find the number under the input max that produces the longest chain
 		while moving along a Collatz sequence
 	'''
@@ -6,7 +6,7 @@ def Euler_14(maxx=1000000):
 		'''Returns the number of terms in the collatz sequence of the given number
 		'''
 		retval = 0
-		while x is not 1:
+		while x != 1:
 			x = 3*x + 1 if x%2 else x//2
 			retval += 1
 		return retval

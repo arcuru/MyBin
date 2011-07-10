79a595a8f5ea82c098b4793b17a5ad67d656873a
Patrick Jackson <PatrickSJackson@gmail.com>
2011-07-10 04:52:04 -0400

    Upgraded test capabilities for Cpp primes.

--- Cpp_Projects/Primes/src/test.cc
+++ Cpp_Projects/Primes/src/test.cc
@@ -1,10 +1,11 @@
 #include "Primes.h"
 #include <iostream>
+#include <cstring>
 using namespace std;
 
-int main()
+int main(int argc, char** argv)
 {
-	Primes p(100000000);
+	Primes p(atoi(argv[1]));
 	int* list = p.getList();
 	int count = 0;
 	for (int i = 0; list[i] != 0; i++) {

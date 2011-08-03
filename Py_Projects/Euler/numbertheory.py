91f84c32811419cad612a21d8564931699cdc2ac
Patrick Jackson <PatrickSJackson@gmail.com>
2011-08-02 21:05:38 -0400

    Fixed range error in Python phi() implementation.

--- Py_Projects/Euler/numbertheory.py
+++ Py_Projects/Euler/numbertheory.py
@@ -4,8 +4,8 @@ def phi(n):
 	'''
 	phiList = [x-1 if x%2==1 else x//2 for x in range(0,n+1)]
 	phiList[1] = 1
-	for x in range(3, n, 2):
+	for x in range(3, n+1, 2):
 		if phiList[x] == x-1:
-			for s in range(2*x, n, x):
+			for s in range(2*x, n+1, x):
 				phiList[s] -= phiList[s] // x
 	return phiList

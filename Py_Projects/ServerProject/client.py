250e340501dc3c930856811ae8beefd8ae23d1c5
Patrick Jackson <PatrickSJackson@gmail.com>
2011-09-04 14:06:18 -0400

    Updates to the Server project code.

--- Py_Projects/ServerProject/client.py
+++ Py_Projects/ServerProject/client.py
@@ -7,21 +7,21 @@ import socket
 import sys
 
 host = 'localhost'
-port = 50001
+port = 50003
 size = 1024
 s = None
 try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.connect((host, port))
+	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+	s.connect((host, port))
 except socket.error:
-    if s:
-        s.close()
-    print('Could not open socket:', message)
-    sys.exit(1)
-data = 'uname -a'
+	if s:
+		s.close()
+	print('Could not open socket:', message)
+	sys.exit(1)
+data = 'pwd'
 ret = s.send(bytearray(data,'utf-8'))
 if (ret is not len(data)):
-    print(ret)
+	print(ret)
 data = s.recv(size).decode()
 s.close()
 print('Received:')
--- Py_Projects/ServerProject/server.py
+++ Py_Projects/ServerProject/server.py
@@ -5,12 +5,33 @@ A simple echo server that handles some exceptions
 
 import socket
 import subprocess
+from multiprocessing import Process
+
+def data_handler(data, client):
+	print("Command")
+	print(data)
+	# Break data into list of arguments
+	args = data.split(' ')
+	try:
+		proc = subprocess.Popen(args, stdout=subprocess.PIPE,
+			stderr=subprocess.PIPE)
+		so, se = proc.communicate()
+		print("Result:")
+		print(so.decode())
+		if data:
+			client.send(bytearray(so.decode(),'utf-8'))
+		client.close()
+	except:
+		print("Error in processing")
+		client.send(bytearray("Error in processing",'utf-8'))
+		client.close()
 
 host = ''
-port = 50001
+port = 50003
 backlog = 5
 size = 1024
 s = None
+proc_list = []
 try:
 	s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 	s.bind((host, port))
@@ -23,17 +44,14 @@ except socket.error:
 while True:
 	client, address = s.accept()
 	data = client.recv(size).decode()
-	print("Command")
-	print(data)
-	# Break data into list of arguments
-	args = data.split(' ')
+	# Break off child process to deal with this command
+	proc_list.append(Process(target=data_handler, args=(data,client)))
+	proc_list[-1].start()
+	count = 0
+	for x in proc_list:
+		if x.is_alive():
+			count += 1
+		else:
+			proc_list.remove(x)
 
-	proc = subprocess.Popen(args, stdout=subprocess.PIPE,
-			stderr=subprocess.PIPE)
-	so, se = proc.communicate()
-	print("Result:")
-	print(so.decode())
-	if data:
-		client.send(bytearray(so.decode(),'utf-8'))
-	client.close()
 

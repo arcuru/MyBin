8917181396120b1056d1a076c5946d5c8c481c76
Patrick Jackson <PatrickSJackson@gmail.com>
2011-11-27 01:27:24 -0500

    Met user interface requirements for MBSet.

--- Cpp_Projects/MBSet/MBSet.cc
+++ Cpp_Projects/MBSet/MBSet.cc
@@ -33,6 +33,11 @@ double init_XMin = -2.0;
 double init_YMin = -1.2;
 double init_XMax = 1.0;
 double init_YMax = 1.8;
+bool click = false;
+int click_XMin;
+int click_XMax;
+int click_YMin;
+int click_YMax;
 int      maxIt = 2000;     //!< Max iterations for the set computations
 
 //GLfloat palette[5][3] = {{1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {0.0, 0.0, 1.0}, {0.5, 0.5, 0.5}, {1.0, 1.0, 1.0}}; 
@@ -51,7 +56,8 @@ struct MBWindow {
 	GLfloat* palette;      //!< Color palette for this window
 };
 
-MBWindow window;  //!< Struct holding the properties of the current window
+MBWindow window_list[100]; //!< List containing all the drawn windows
+size_t window = 0;  //!< Index of current working window
 
 /** Iterate Point
  *  returns the number of iterations below max_iter that it took to reach
@@ -161,18 +167,59 @@ void display(void)
 	midy *= -1;
 	glTranslated(midx, midy, 0);
 */
+	// Get a pointer to the current window
+	MBWindow* w = &window_list[window];
+
+	// Scale Min and Max points for correct aspect ratio
+	double pix_scale = ((double)w->height) / w->width;
+	double point_scale = (w->YMax - w->YMin) / (w->XMax - w->XMin);
+	if (pix_scale != point_scale) {
+		double dy = w->YMax - w->YMin;
+		double dx = w->XMax - w->XMin;
+		double x_pix_size = dx / ((double)w->width);
+		double y_pix_size = dy / ((double)w->height);
+		if (x_pix_size < y_pix_size) {
+			// Modify X vals
+			double mid = (w->XMax + w->XMin) / 2.0;
+			w->XMin = mid - (y_pix_size * w->width / 2.0);
+			w->XMax = mid + (y_pix_size * w->width / 2.0);
+		}
+		else if (x_pix_size > y_pix_size) {
+			// Modify Y vals
+			double mid = (w->YMax + w->YMin) / 2.0;
+			w->YMin = mid - (x_pix_size * w->height / 2.0);
+			w->YMax = mid + (x_pix_size * w->height / 2.0);
+			cout << w->YMin << endl;
+			cout << w->YMax << endl;
+		}
+	}
+
 	// Calculate mandelbrot window
-	mandelbrot_compute(&window);
+	mandelbrot_compute(w);
 
 	// Correct for 1 pixel discrepency
 	glTranslatef(1, 0, 0);
 
 	// Iterate over calculated image and display points
 	glBegin( GL_POINTS );
-	for (int r = 0; r < window.height; r++) {
-		for (int c = 0; c < window.width; c++) {
-			glColor3fv(&window.palette[3 * window.Iters[window.width * r + c]]);
+	for (int r = 0; r < w->height; r++) {
+		for (int c = 0; c < w->width; c++) {
+			glColor3fv(&w->palette[3 * w->Iters[w->width * r + c]]);
 			glVertex2d(c, r);
+		}
+	}
+	if (click) {
+		glColor3f(1.0, 0.0, 0.0);
+		cout << "Coords" << endl;
+		cout << click_XMin << ", " << click_XMax << endl;
+		cout << click_YMin << ", " << click_YMax << endl;
+		for (int i = click_XMin; i != click_XMax; i += (click_XMax - click_XMin) / abs(click_XMax - click_XMin)) {
+			glVertex2d(i, w->height - click_YMin);
+			glVertex2d(i, w->height - click_YMax);
+		}
+		for (int i = click_YMin; i != click_YMax; i += (click_YMax - click_YMin) / abs(click_YMax - click_YMin)) {
+			glVertex2d(click_XMin, w->height - i);
+			glVertex2d(click_XMax, w->height - i);
 		}
 	}
 	glEnd();
@@ -209,15 +256,16 @@ void init()
 	}
 
 	// Initialize current window defaults
-	window.XMin = init_XMin;
-	window.YMin = init_YMin;
-	window.XMax = init_XMax;
-	window.YMax = init_YMax;
-	window.maxIters = maxIt;
-	window.width = init_width;
-	window.height = init_height;
-	window.Iters = NULL;
-	window.palette = default_palette;
+	MBWindow* w = &window_list[window];
+	w->XMin = init_XMin;
+	w->YMin = init_YMin;
+	w->XMax = init_XMax;
+	w->YMax = init_YMax;
+	w->maxIters = maxIt;
+	w->width = init_width;
+	w->height = init_height;
+	w->Iters = NULL;
+	w->palette = default_palette;
 }
 
 /** Reshape
@@ -231,11 +279,13 @@ void reshape(int w, int h)
 #ifdef DEBUG
 	cout << "Reshape: (" << w << ", " << h << ")" << endl;
 #endif
-	window.width = w;
-	window.height = h;
-	if (NULL != window.Iters)
-		free(window.Iters);
-	window.Iters = NULL;
+	// Get pointer to current window object
+	MBWindow* win = &window_list[window];
+	win->width = w;
+	win->height = h;
+	if (NULL != win->Iters)
+		free(win->Iters);
+	win->Iters = NULL;
 	// Resize viewport and ortho projection
 	glViewport(0,0, (GLsizei)w, (GLsizei)h);
 	glMatrixMode(GL_PROJECTION);
@@ -260,30 +310,51 @@ void mouse(int button, int state, int x, int y)
 		firstvalid = true;
 		firstx = x;
 		firsty = y;
+		click_XMin = x;
+		click_YMin = y;
+		click_XMax = x;
+		click_YMax = y;
+		click = true;
 	}
 	if (state && firstvalid) {
-		MBWindow w;
-		w.YMin = (((double)window.height - y) / ((double) window.height)) * (window.YMax - window.YMin) + window.YMin;
-		w.YMax = (((double)window.height - firsty) / ((double) window.height)) * (window.YMax - window.YMin) + window.YMin;
-		w.XMin = (((double)firstx) / ((double) window.width)) * (window.XMax - window.XMin) + window.XMin;
-		w.XMax = (((double)x) / ((double) window.width)) * (window.XMax - window.XMin) + window.XMin;
-		
-		// Ugly hack to keep it square
-		double dx = w.XMax - w.XMin;
-		double dy = w.YMax - w.YMin;
-		double min = (dx < dy) ? dx : dy;
-		w.XMax = w.XMin + min;
-		w.YMax = w.YMin + min;
-		
+		click = false;
 
-		w.width = window.width;
-		w.height = window.height;
-		w.maxIters = window.maxIters;
-		w.Iters = NULL;
-		w.palette = default_palette;
-		if (NULL != window.Iters)
-			free(window.Iters);
-		window = w;
+		// Calculate pixel coordinates
+		int min = (abs(x - click_XMin) < abs(y - click_YMin)) ? x-click_XMin : y-click_YMin;
+		if (0 == min)
+			return;
+		min = abs(min);
+		click_XMax = click_XMin + (min * ((x - click_XMin) / abs(x-click_XMin)));
+		click_YMax = click_YMin + (min * ((y - click_YMin) / abs(y-click_YMin)));
+
+		MBWindow* w = &window_list[window+1];
+		MBWindow* wtmp = &window_list[window];
+		w->YMin = (((double)wtmp->height - click_YMax) / ((double) wtmp->height)) * (wtmp->YMax - wtmp->YMin) + wtmp->YMin;
+		w->YMax = (((double)wtmp->height - click_YMin) / ((double) wtmp->height)) * (wtmp->YMax - wtmp->YMin) + wtmp->YMin;
+		w->XMin = (((double)click_XMin) / ((double) wtmp->width)) * (wtmp->XMax - wtmp->XMin) + wtmp->XMin;
+		w->XMax = (((double)click_XMax) / ((double) wtmp->width)) * (wtmp->XMax - wtmp->XMin) + wtmp->XMin;
+		
+		// Ensure that we have the min and max set up as lower left and upper right corners
+		if (w->YMax < w->YMin) {
+			// Swap
+			double tmp = w->YMax;
+			w->YMax = w->YMin;
+			w->YMin = tmp;
+		}
+		if (w->XMax < w->XMin) {
+			// Swap
+			double tmp = w->XMax;
+			w->XMax = w->XMin;
+			w->XMin = tmp;
+		}
+
+		// Inherit attributes from old window
+		w->width = wtmp->width;
+		w->height = wtmp->height;
+		w->maxIters = wtmp->maxIters;
+		w->Iters = NULL;
+		w->palette = wtmp->palette;
+		window++;
 		glutPostRedisplay();
 	}
 }
@@ -293,6 +364,13 @@ void motion(int x, int y)
 #ifdef DEBUG
 	cout << "Motion: (" << x << ", " << y << ")" << endl;
 #endif
+	int min = (abs(x - click_XMin) < abs(y - click_YMin)) ? x-click_XMin : y-click_YMin;
+	if (0 == min)
+		return;
+	min = abs(min);
+	click_XMax = click_XMin + (min * ((x - click_XMin) / abs(x-click_XMin)));
+	click_YMax = click_YMin + (min * ((y - click_YMin) / abs(y-click_YMin)));
+	glutPostRedisplay();
 }
 
 void keyboard(unsigned char c, int x, int y)
@@ -300,6 +378,17 @@ void keyboard(unsigned char c, int x, int y)
 #ifdef DEBUG
 	cout << "Keyboard: (" << x << ", " << y << ") " << c << endl;
 #endif
+	
+	switch ( c ) {
+		case 'b':	
+			if (window > 0) // Restrict to positive values
+				window--;
+			glutPostRedisplay();
+			break;
+
+		default:	
+			break;
+	}
 }
 
 int main(int argc, char** argv)

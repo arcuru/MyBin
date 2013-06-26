bc53d096e709222ae8b7c890afc60c14d4fddcbe
Patrick Jackson <PatrickSJackson@gmail.com>
2013-06-26 02:43:26 +0000

    Updated Euler 14 for caching, formatting, and other improvements.

--- C_Projects/Euler/src/14.c
+++ C_Projects/Euler/src/14.c
@@ -2,25 +2,34 @@
 
 int64_t Euler_14()
 {
-	int maxval = 1000000;
-    int longest = 0, longval = 0;
-    int count;
-//#pragma omp parallel for
-	for (count = maxval-1; count > maxval/2; count-=2) {
-        int length = 0;
-        uint64_t series = count;
-        while (series != 1) {
-            if (series % 2 == 0) {
-                series = series / 2;
-            } else {
-                series = 3 * series + 1;
-            }
-            ++length;
-        }
-        if (length > longest) {
-            longest = length;
-            longval = count;
-        }
-    }
-    return longval;
+	const int maxval = 1000000;
+	int longest = 0, longval = 0;
+	int count;
+	int16_t cache[maxval];
+	//#pragma omp parallel for
+	for (count = 1; count <= maxval; ++count) {
+		int length = 0;
+		int64_t series = count;
+		while (series != 1) {
+			if ( series < count ) {
+				length += cache[series];
+				break;
+			}
+			if (series % 2 == 0) {
+				series = series / 2;
+				++length;
+			} else {
+				series = 3 * series + 1;
+				series /= 2;
+				length += 2;
+			}
+		}
+		cache[count] = length;
+		if (length > longest) {
+			longest = length;
+			longval = count;
+		}
+	}
+	// Longest is 524
+	return longval;
 }

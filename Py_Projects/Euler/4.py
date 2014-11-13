3a55dacc5128d60fe9f7b297ef1a940dd25393a9
Patrick Jackson <PatrickSJackson@gmail.com>
2014-11-12 20:42:47 -0800

    Adding Euler #4 in Python

---
+++ Py_Projects/Euler/4.py
@@ -0,0 +1,44 @@
+
+def Euler_4(n=3):
+	''' Find the largest palindrome made from the product of two
+		n-digit numbers
+	'''
+
+	# Set an initial value of 0 for maxval
+	maxval = 0
+
+	for a in range (10**n-1, 0, -1):
+		for b in range (10**n-1, 0, -1):
+			if maxval > a*b:
+				break
+			if isPalindrome(a*b):
+				maxval = a*b
+				break
+	return maxval
+
+def reverseNumber(number):
+	''' Reverses the base 10 representation of a number
+		returns the new value
+
+		Requires that number is an integer
+	'''
+
+	rev = 0
+	while number > 0:
+		rev *= 10
+		rev += number % 10
+		number //= 10
+	return rev
+
+
+def isPalindrome(number):
+	''' Return true if a number is a palindrome
+		False otherwise
+	'''
+
+	if number == reverseNumber(number):
+		return True
+	return False
+
+if __name__ == '__main__':
+	print(Euler_4())

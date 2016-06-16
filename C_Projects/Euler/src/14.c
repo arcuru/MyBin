8a040360b426823a2faa52022eab7ad21a88b7e8
Patrick Jackson <PatrickSJackson@gmail.com>
2016-06-16 10:50:31 -0700

    Updated the caching for Euler 14

--- C_Projects/Euler/src/14.c
+++ C_Projects/Euler/src/14.c
@@ -7,31 +7,39 @@ int64_t Euler_14()
 	int longest = 0, longval = 0;
 	int count;
 	int16_t* cache = (int16_t*) calloc(sizeof(int16_t), maxval + 1);
-	//#pragma omp parallel for
 	for (count = 1; count <= maxval; ++count) {
-		int length = 0;
-		int64_t series = count;
-		while (series != 1) {
-			if ( series < count ) {
-				length += cache[series];
-				break;
+		// Caching results yields a lot of speedup
+		int length = cache[count];
+		if (length == 0) {
+			int64_t series = count;
+			while (series != 1) {
+				if ( series < count ) {
+					length += cache[series];
+					break;
+				}
+				if (series % 2 == 0) {
+					series = series / 2;
+					++length;
+				} else {
+					series = 3 * series + 1;
+					series /= 2;
+					length += 2;
+				}
 			}
-			if (series % 2 == 0) {
-				series = series / 2;
-				++length;
-			} else {
-				series = 3 * series + 1;
-				series /= 2;
-				length += 2;
+			cache[count] = length;
+			int tmp = count;
+			int tmplen = length;
+			while (tmp < maxval / 2) {
+				tmp *= 2;
+				cache[tmp] = ++tmplen;
 			}
 		}
-		cache[count] = length;
 		if (length > longest) {
 			longest = length;
 			longval = count;
 		}
 	}
-	// Longest is 524
+	// Longest is 524 for <1,000,000
 	free( cache );
 	return longval;
 }

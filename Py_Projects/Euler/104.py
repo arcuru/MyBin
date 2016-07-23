7885a041ad58fd97ab11a533436c2569ffe81688
Patrick Jackson <PatrickSJackson@gmail.com>
2016-07-23 12:27:31 -0700

    Cleaned up py/euler/104

--- Py_Projects/Euler/104.py
+++ Py_Projects/Euler/104.py
@@ -1,43 +1,36 @@
-import gmpy2
 def Euler_104():
 	'''Find the index of the first Fibonacci number with first and last 9 digits pandigital
 	'''
 	# a is lower fib number, b is higher fib number
 	# index is the fibonacci index of b
 
-	(a, b, index) = (gmpy2.mpz(1), gmpy2.mpz(1), 2)
+	(a, b, index) = ([1, 1], [1, 1], 2)
 	while True:
-		(a, b, index) = (b, a + b, index + 1)
-		low_num = b % (10 ** 9)
-		high_num = b
-		if isPandigital(low_num):
-			while high_num > 10 ** 9:
-				if high_num > 10 ** 50:
-					if high_num > 10 ** 500:
-						if high_num > 10 ** 5000:
-							high_num //= 10 ** 4500
-						high_num //= 10 ** 450
-					high_num //= 10 ** 41
-				high_num //= 10
-			if isPandigital(high_num):
-				return index
+		(a, b, index) = (b, [a[0]+b[0], a[1]+b[1]], index + 1)
 
-def isPandigital(number):
+		# Modify the numbers to track just the low and high digits
+		if b[0] > 10**9:
+			b[0] %= 10**9
+		while b[1] > 10**20:
+			b[1] //= 10
+			a[1] //= 10
+
+		if not is_Pandigital(b[0]):
+			continue
+
+		if is_Pandigital(int(str(b[1])[:9])):
+			return index
+
+pan_set = set(map(str, range(1,10)))
+
+def is_Pandigital(number):
 	'''Return true if a number is pandital (contains 1-9 exactly once)
 		False otherwise
 	'''
 	if number < 10 ** 8 or number >= 10 ** 9:
 		return False
-	count_list = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-	while number > 0:
-		if number % 10 == 0:
-			return False
-		if count_list[(number % 10)]  == 0:
-			count_list[(number % 10)] = 1
-			number //= 10
-		else:
-			return False
-	return True
+	return set(str(number)) == pan_set
 
 if __name__ == '__main__':
 	print(Euler_104())
+
